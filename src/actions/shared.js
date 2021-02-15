810482ee2beb781bef5df39e7194a64371edbd73
Barbara Peric <b.peric10@gmail.com>
2021-02-15 13:27:26 +0100

    FEAT: shared data redux action

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,17 +1,22 @@
 import { showLoading, hideLoading } from 'react-redux-loading-bar'
 import { getInitialData } from '../utils/api'
+import { receiveTweets } from './tweets'
+import { receiveUser } from './users'
+import { setAuthedUser } from './authedUser'
 export const SET_INITIAL_DATA = "SET_INITIAL_DATA"
 
-export const setInitialData = ({ tweets, users}) => {
-  return {
-    type: SET_INITIAL_DATA,
-    tweets,
-    users
-  }
-}
+const AUTHED_USER = 'tylermcginnis'
 
 export const handleSetInitialData = () => {
   return (dispatch) => {
     dispatch(showLoading())
+
+    return getInitialData()
+      .then(({ tweets, users }) => {
+        dispatch(receiveTweets(tweets))
+        dispatch(receiveUser(users))
+        dispatch(setAuthedUser(AUTHED_USER))
+        dispatch(hideLoading())
+      })
   }
 }
